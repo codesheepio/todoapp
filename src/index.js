a063d17cac55a5a860439721697af5ec1da58906
Arnupharp Viratanapanu <arnupharp@larngeartech.com>
2016-10-05 16:49:57 +0700

    Add TodoInput

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import TodoInput from './components/TodoInput'
 import TodoList from './components/TodoList'
 import styles from './styles.css'
 
@@ -8,6 +9,11 @@ const todos = [
   { text: "read book", complete: true},
   { text: "go for walk", complete: false},
 ]
-const App = <TodoList todos={todos} />
+const App = (
+  <div>
+    <TodoInput />
+    <TodoList todos={todos} />
+  </div>
+)
 ReactDOM.render(App,
   document.getElementById('react-root'))
